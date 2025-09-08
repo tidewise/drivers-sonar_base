7d8fa9dd9f377fd1f413ea13217e7494f57abe99
seixasxbr <mateus.seixas@tidewise.io>
2025-09-08 17:17:58 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,16 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace sonar_base {
+    class DummyClass {
+    public:
+        /**
+         * Print a welcome to stdout
+         * \return nothing
+         */
+        void welcome();
+    };
+
+} // end namespace sonar_base
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <sonar_base/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    sonar_base::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
